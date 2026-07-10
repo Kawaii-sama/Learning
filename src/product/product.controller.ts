e38b7f8cb65f6570a1421927c23c564d40ed5b11
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-10 17:14:54 +0530

    learning controllers

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor (private readonly productService: ProductService){}
+
+    @Get()
+    getProducts() {
+        return this.productService.getAllProducts();
+    }
+
+    @Get(:'id')
+    getProduct(@Param('id') id: string) {
+        return this.productService.getProductById(Number(id))
+    }
+}
