5a22cb3dae2e20f2ba93fd477d62f1e64a112f60
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-11 18:25:16 +0530

    learning

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { StudentService } from './student.service';
 
 @Controller('student')
@@ -13,5 +13,10 @@ export class StudentController {
     @Get(':id')
     getOne(@Param('id') id : string) {
         return this.studentService.getStudentById(Number (id));
+    }
+
+    @Post()
+    create(@Body() body : { name : string; age : number}) {
+        return this.studentService.createStudent(body);
     }
 }
